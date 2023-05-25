47ee57de9e11281b2497be5151a231242c17572d
Sharminaktermetu <sharminacc1@gmail.com>
2023-05-26 00:00:26 +0600

    get single data

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const cors = require('cors')
 const app =express();
 const port =process.env.port||5000
 app.use(cors())
+
 const data= require('./data/data.json')
+
 app.get("/",(req,res)=>{
    res.send("Server is running")
 });
 app.get("/data",(req,res)=>{
    res.send(data)
+});
+
+
+app.get('/data/:id',(req,res)=>{
+      const id =req.params.id;
+      console.log(id);
+      const selectedChef =data.find(n=>n.id==id)
+      res.send(selectedChef)
 });
 
 app.listen(port,()=>{
